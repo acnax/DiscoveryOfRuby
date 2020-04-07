4084ccc15e781455261fb26b000b390e3f4202f2
acnax <alexandrelacroix.dev@gmail.com>
2020-04-07 22:13:36 +0200

    pyramide

--- pyramide.rb
+++ pyramide.rb
@@ -2,38 +2,56 @@
 
 
 ##################   la boucle de Champion   ##################
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+x=gets.chomp.to_i
 
 
-begin
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu (1 à 25) ?"
-	print " > "
-	num = gets.chomp.to_i
-end until(num > 0 && num < 26)
+blank = x - 1 
 
-nb_space = num -1
-nb_block = 1
+puts "Voici ta pyramide :"
 
-while(num > 0)
-	ligne_tmp = ""
-	space_tmp = nb_space
-	block_tmp = nb_block
+x.times do |i|
+		hashtag=" "
+    (x-1-i).times do
+    hashtag = print " "
+    end
+
+    (i+1).times do
+    hashtag = print "#"
+    end
+		puts hashtag
+end
+
+#begin
+#	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu (1 à 25) ?"
+#	print " > "
+#	num = gets.chomp.to_i
+#end until(num > 0 && num < 26)
+
+#nb_space = num -1
+#nb_block = 1
+
+#while(num > 0)
+#	ligne_tmp = ""
+#	space_tmp = nb_space
+#	block_tmp = nb_block
 		
-	while(space_tmp > 0)
-		ligne_tmp = ligne_tmp + " "
-		space_tmp -= 1
-	end
+#	while(space_tmp > 0)
+#		ligne_tmp = ligne_tmp + " "
+#		space_tmp -= 1
+#	end
 		
-	while(block_tmp > 0)
-		ligne_tmp = ligne_tmp + "#"
-		block_tmp -= 1
-	end
-		
-	puts ligne_tmp
-	num -=1
-	nb_space -= 1
-	nb_block += 1
-end 
-
+#	while(block_tmp > 0)
+#		ligne_tmp = ligne_tmp + "#"
+#		block_tmp -= 1
+#	end
+#		
+#	puts ligne_tmp
+#	num -=1
+#	nb_space -= 1
+#	nb_block += 1
+#end 
+#
 # Methode plus facile
 
 #etage.times do |i|
